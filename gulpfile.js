7f7fc1f6ac405f7e0fa7824b55f108b5c40469e7
zhouwenbin <330759294@qq.com>
2015-11-25 15:58:11 +0800

    双引号

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp'),
-    eslint = require('gulp-eslint'),
-    mocha = require('gulp-mocha');
+var gulp = require("gulp"),
+    eslint = require("gulp-eslint"),
+    mocha = require("gulp-mocha");
 
-gulp.task('lint', function () {
-  return gulp.src(['index.js', 'test/*.js', 'gulpfile.js'])
+gulp.task("lint", function () {
+  return gulp.src(["index.js", "test/*.js", "gulpfile.js"])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('test', function () {
-  return gulp.src('test/*.js', { read: false }).pipe(mocha({reporter: 'nyan'}));
+gulp.task("test", function () {
+  return gulp.src("test/*.js", { read: false }).pipe(mocha({reporter: "nyan"}));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('**/*.js', ['check'] );
+gulp.task("watch", function() {
+  gulp.watch("**/*.js", ["check"] );
 });
 
-gulp.task('check', ['lint', 'test']);
-gulp.task('default', ['check', 'watch']);
+gulp.task("check", ["lint", "test"]);
+gulp.task("default", ["check", "watch"]);
